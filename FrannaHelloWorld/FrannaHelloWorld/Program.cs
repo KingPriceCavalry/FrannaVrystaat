2f2c6e9a478e6e34b1f1de40be8e6b72cdd6357a
FrannaVrystaat <francois.rademan3@kingprice.co.za>
2014-08-26 14:11:10 +0200

    project added correctly
    :P

---
+++ FrannaHelloWorld/FrannaHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrannaHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Franna Hello World !");
+            Console.ReadLine();
+        }
+    }
+}
